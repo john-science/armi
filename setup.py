8a6ef899773d4aa3c0177082222281f410eb9678
John Stilley <1831479+john-science@users.noreply.github.com>
2022-11-03 13:09:50 -0700

    Attempting mpi4py fix for Python 3.11

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         "pyDOE",
     ],
     extras_require={
-        "mpi": ["mpi4py"],
+        "mpi": ["mpi4py>=3.1.4"],
         "grids": ["wxpython<=4.1.1"],
         "memprof": ["psutil"],
         "dev": [
